945c3bd8a63b1b9d698e766f277870e2b9d31be2
Luís <luis.longof@gmail.com>
2026-09-14 00:10:15 -0300

    Changes

--- src/domain/repositories/IProjectRepository.ts
+++
@@ -1,9 +0,0 @@
-import type { Project, CreateProjectInput } from '../Project'
-
-export interface IProjectRepository {
-  findAll(): Promise<Project[]>
-  findById(id: number): Promise<Project | null>
-  save(input: CreateProjectInput): Promise<Project>
-  update(project: Project): Promise<Project>
-  delete(id: number): Promise<void>
-}
